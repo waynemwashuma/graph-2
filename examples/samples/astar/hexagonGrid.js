825fd0f1636af2e23b48762543d5f09e587bab2a
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2026-02-18 06:45:47 +0300

    Correct ts type issues

--- examples/samples/astar/hexagonGrid.js
+++ examples/samples/astar/hexagonGrid.js
@@ -1,9 +1,13 @@
-import { Vector2, rand } from './vector2.js';
-import {} from '../utils.js'
-import { Graph } from './graph.js';
-import { aStar,dijkstra } from "./algo/index.js"
+import { Vector2 } from '../../../vector2.js';
+import { drawGraph, drawPath, generateHexagonalNodes, } from '../utils.js'
+import { Graph } from '../../../graph.js';
+import { aStar } from "../../../algo/index.js"
 const canvas = document.createElement("canvas")
 const ctx = canvas.getContext('2d')
+
+if(!ctx){
+  throw "No canvas 2d context!"
+}
 
 canvas.width = innerWidth
 canvas.height = innerHeight
@@ -15,10 +19,10 @@ document.body.appendChild(canvas)
  */
 const graph = new Graph(false)
 
-generateDiagonalBoxedNodes(graph, innerWidth - 100, innerHeight - 100, new Vector2(15, 15), new Vector2(50, 50))
+generateHexagonalNodes(graph, innerWidth - 100, innerHeight - 100, new Vector2(15, 15), new Vector2(50, 50))
 const start = 0
 const end = 104
-const path = aStar(graph,Vector2.distanceTo, start, end)
+const path = aStar(graph, Vector2.distanceTo, start, end)
 
-drawGraph(graph)
-drawPath(graph, path.path(end))
+drawGraph(ctx, graph)
+drawPath(ctx, graph, path.path(end))
--- examples/samples/utils.js
+++ examples/samples/utils.js
@@ -1,47 +1,62 @@
+import { Graph } from "../../graph.js"
+import { Vector2, rand } from "../../vector2.js"
+
 /**
+ * @param {CanvasRenderingContext2D} ctx
  * @param {Graph<Vector2>} graph
  */
-export function drawGraph(graph) {
+export function drawGraph(ctx,graph) {
+  const edges = graph.getEdges()
+  const nodes = graph.getNodes()
   ctx.strokeStyle = "lightgreen"
   
-  for (let i = 0; i < graph.edges.length; i++) {
-    const edge = graph.edges[i]
-    const pos1 = graph.nodes[edge.from].weight
-    const pos2 = graph.nodes[edge.to].weight
+  for (let i = 0; i < edges.length; i++) {
+    const edge = edges[i]
+    const pos1 = graph.getNode(edge.from)?.weight
+    const pos2 = graph.getNode(edge.to)?.weight
     
-    ctx.beginPath()
-    ctx.moveTo(...pos1)
-    ctx.lineTo(...pos2)
-    ctx.stroke()
-    ctx.closePath()
+    if(pos1 && pos2){ 
+      ctx.beginPath()
+      ctx.moveTo(pos1.x,pos1.y)
+      ctx.lineTo(pos2.x,pos2.y)
+      ctx.stroke()
+      ctx.closePath()
+    }
   }
   ctx.fillStyle = "white"
-  for (let i = 0; i < graph.nodes.length; i++) {
-    const position = graph.nodes[i].weight
+  for (let i = 0; i < nodes.length; i++) {
+    const position = nodes[i].weight
     ctx.beginPath()
-    ctx.arc(...position, 3, 0, Math.PI * 2)
+    ctx.arc(position.x,position.y, 3, 0, Math.PI * 2)
     ctx.fill()
     ctx.closePath()
   }
 }
 
 /**
+ * @param {CanvasRenderingContext2D} ctx
  * @param {Graph<Vector2>} graph
- * @param {NodeId} path
+ * @param {import("../../graph").NodeId[]} path
  */
-function drawPath(graph, path) {
+export function drawPath(ctx,graph, path) {
   if (!path.length) {
     return
   }
   ctx.beginPath()
   ctx.strokeStyle = "red"
-  const start = graph.getNode(path[0])
-  ctx.moveTo(...start.weight)
+  const start = graph.getNode(path[0])?.weight
+
+  if(!start){
+    return
+  }
+  ctx.moveTo(start.x,start.y)
   for (let i = 1; i < path.length; i++) {
     const id = path[i]
-    const node = graph.getNode(id)
+    const node = graph.getNode(id)?.weight
     
-    ctx.lineTo(...node.weight)
+    if (node) {
+      ctx.lineTo(node.x,node.y)
+    }
   }
   ctx.stroke()
   ctx.closePath()
@@ -49,8 +64,12 @@ function drawPath(graph, path) {
 
 /**
  * @param {Graph} graph
+ * @param {number} width
+ * @param {number} height
+ * @param {Vector2} num
+ * @param {Vector2} offset
  */
-function generateBoxedNodes(graph, width, height, num, offset = new Vector2()) {
+export function generateBoxedNodes(graph, width, height, num, offset = new Vector2()) {
   const widthX = width / num.x
   const heightY = height / num.y
   
@@ -71,29 +90,36 @@ function generateBoxedNodes(graph, width, height, num, offset = new Vector2()) {
     for (let y = 0; y < num.y - 1; y++) {
       graph.addEdge(
         x + y * num.x,
-        x + (y + 1) * num.x
+        x + (y + 1) * num.x,
+        undefined
       )
       graph.addEdge(
         x + y * num.x,
-        x + y * num.x + 1
+        x + y * num.x + 1,
+        undefined
       )
     }
   }
   for (let x = (num.y - 1) * (num.x); x < num.y * num.x - 1; x++) {
-    graph.addEdge(x, x + 1)
+    graph.addEdge(x, x + 1, undefined)
   }
   for (let y = num.y - 1; y < num.y * num.x - num.x; y += num.y) {
     graph.addEdge(
       y,
-      y + num.y
+      y + num.y,
+      undefined
     )
   }
 }
 
 /**
  * @param {Graph} graph
+ * @param {number} width
+ * @param {number} height
+ * @param {Vector2} num
+ * @param {Vector2} offset
  */
-function generateHexagonalNodes(graph, width, height, num, offset = new Vector2()) {
+export function generateHexagonalNodes(graph, width, height, num, offset = new Vector2()) {
   const widthX = width / num.x
   const heightY = height / num.y
   num.x += 1
@@ -101,10 +127,10 @@ function generateHexagonalNodes(graph, width, height, num, offset = new Vector2(
   
   for (let y = 0; y < num.y; y++) {
     for (let x = 0; x < num.x; x++) {
-      if (y % 2 === 1 & x % 2 === 0) {
+      if (y % 2 === 1 && x % 2 === 0) {
         continue
       }
-      if (y % 2 === 0 & x % 2 === 1) {
+      if (y % 2 === 0 && x % 2 === 1) {
         continue
       }
       graph.addNode(
@@ -135,8 +161,12 @@ function generateHexagonalNodes(graph, width, height, num, offset = new Vector2(
 
 /**
  * @param {Graph} graph
+ * @param {number} width
+ * @param {number} height
+ * @param {Vector2} num
+ * @param {Vector2} offset
  */
-function generateDiagonalBoxedNodes(graph, width, height, num, offset = new Vector2()) {
+export function generateDiagonalBoxedNodes(graph, width, height, num, offset = new Vector2()) {
   const widthX = width / num.x
   const heightY = height / num.y
   
@@ -157,37 +187,44 @@ function generateDiagonalBoxedNodes(graph, width, height, num, offset = new Vect
     for (let y = 0; y < num.y - 1; y++) {
       graph.addEdge(
         x + y * num.x,
-        x + (y + 1) * num.x
+        x + (y + 1) * num.x,
+        undefined
       )
       graph.addEdge(
         x + y * num.x,
-        x + y * num.x + 1
+        x + y * num.x + 1,
+        undefined
       )
     }
   }
   for (let x = (num.y - 1) * (num.x); x < num.y * num.x - 1; x++) {
-    graph.addEdge(x, x + 1)
+    graph.addEdge(x, x + 1, undefined)
   }
   for (let y = num.y - 1; y < num.y * num.x - num.x; y += num.y) {
     graph.addEdge(
       y,
-      y + num.y
+      y + num.y,
+      undefined
     )
   }
   for (let x = 0; x < num.x - 1; x++) {
     for (let y = 0; y < num.y - 1; y++) {
       graph.addEdge(
         x + y * num.x,
-        x + (y + 1) * num.x + 1
+        x + (y + 1) * num.x + 1,
+        undefined
       )
     }
   }
 }
 
 /**
- * @param {Graph} graph
+ * @param {Graph<Vector2, undefined>} graph
+ * @param {number} number
+ * @param {number} width
+ * @param {number} height
  */
-function generateRandomNodes(graph, number, width, height) {
+export function generateRandomNodes(graph, number, width, height) {
   for (let i = 0; i < number; i++) {
     const node = new Vector2(
       rand(0, width),
@@ -196,13 +233,14 @@ function generateRandomNodes(graph, number, width, height) {
     graph.addNode(node)
   }
   
-  for (let i = 0; i < graph.nodes.length; i++) {
-    for (let j = 0; j < graph.nodes.length; j++) {
+  const nodes = graph.getNodes()
+  for (let i = 0; i < nodes.length; i++) {
+    for (let j = 0; j < nodes.length; j++) {
       if (
         i !== j &&
         rand() >= 0.95
       ) {
-        graph.addEdge(i, j)
+        graph.addEdge(i, j, undefined)
       }
     }
   }
